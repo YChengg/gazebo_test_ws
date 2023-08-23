3ab10b2d515128c51e884831d2aafb4eaede8698
Wyc <a165383@qq.com>
2023-08-23 17:02:55 +0800

    initial project version

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/wyc/gazebo_test_ws/src
+# Build directory: /home/wyc/gazebo_test_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("gazebo_map")
+subdirs("gazebo_nav")
+subdirs("gazebo_pkg")
---
+++ build/gazebo_map/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wyc/gazebo_test_ws/src/gazebo_map
+# Build directory: /home/wyc/gazebo_test_ws/build/gazebo_map
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
